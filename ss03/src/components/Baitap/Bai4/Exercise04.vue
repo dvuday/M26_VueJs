47ec6b0e1de5a661c1cfd0843adc0691fc9060ed
dvudayy <dvuday25@gmail.com>
2024-09-20 16:44:22 +0700

    ss05

---
+++ ss03/src/components/Baitap/Bai4/Exercise04.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div :class="{ active: isActive }" :style="{ backgroundColor: backgroundColor }" class="block">
+      Block
+    </div>
+    <p>isActive={{ isActive }}</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style >
+
+</style>
